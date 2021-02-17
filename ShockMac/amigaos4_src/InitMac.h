f8ffae09cb2a640895f9ab3ee63d540eecee021a
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2021-02-17 17:48:44 +0100

    “Timer.h” exists in AmigaOS4, but its not the same header file.

--- ShockMac/amigaos4_src/InitMac.h
+++ ShockMac/amigaos4_src/InitMac.h
@@ -25,7 +25,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //====================================================================================
 
 
+#ifndef __amigaos4__
 #include <Timer.h>
+#endif
 
 //---------------------
 //  Time Manager defines
