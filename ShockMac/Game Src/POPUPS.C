278978c79d9f830eef445e4ff33035428e2d58da
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2021-02-21 20:43:01 +0100

    File “TextUtils.h” not found, maybe something in MacOS?

--- ShockMac/Game Src/POPUPS.C
+++ ShockMac/Game Src/POPUPS.C
@@ -29,7 +29,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #include <string.h>
+
+#ifndef __amigaos4__
 #include <TextUtils.h>
+#endif
 
 #include "popups.h"
 #include "citres.h"
